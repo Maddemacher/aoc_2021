7a5d4eea624d7d8261634d9c9d994ce938f0b6c5
Emil Bergström <emil.bergstrom@instabox.se>
2021-11-26 16:18:44 +0100

    cleanup

--- day01/app/main.hs
+++ day01/app/main.hs
@@ -3,25 +3,22 @@ import Data.List.Split
 import System.Environment
 import System.Environment.MrEnv
 
-btos :: Bool -> String
-btos x = if x then "True" else "False"
-
 factors :: Integral a => a -> [a]
 factors n = [x | x <- [1 .. n], mod n x == 0]
 
 prime :: Integral a => a -> Bool
 prime n = factors n == [1, n]
 
+notPrime :: Integral a => a -> Bool
+notPrime n = not (prime n)
+
 getRelevantNumbers :: Integral a => (a -> Bool) -> [a] -> [(a, a)]
 getRelevantNumbers predicate numbers = [(x, i) | (x, i) <- zip numbers [0 ..], predicate x]
-
-notPrime :: Integral a => a -> Bool
-notPrime n = factors n /= [1, n]
 
 partOneAggregate :: Integral a => (a, a) -> a
 partOneAggregate (number, index) = number * index
 
-partOne :: [Integer] -> Integer
+partOne :: Integral a => [a] -> a
 partOne numbers = sum (map partOneAggregate (getRelevantNumbers prime numbers))
 
 partTwoAggregate :: Integral a => (a, a) -> a
@@ -30,10 +27,10 @@ partTwoAggregate (number, index) =
     then number
     else - number
 
-partTwo :: [Integer] -> Integer
+partTwo :: Integral a => [a] -> a
 partTwo numbers = sum (map partTwoAggregate (getRelevantNumbers notPrime numbers))
 
-doPart :: String -> [Integer] -> Integer
+doPart :: Integral a => String -> [a] -> a
 doPart "part1" input = partOne input
 doPart "part2" input = partTwo input
 doPart x input = error "Not a valid part"
@@ -43,6 +40,5 @@ main = do
   part <- envAsString "part" ""
   input <- readFile "data/input.txt"
   let numbers = map read (splitOn "\n" input)
-  let work = doPart part numbers
 
-  print work
+  print (doPart part numbers)
