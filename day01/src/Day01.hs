9855ef4a2fc014445890ad6241b8d5f9acb4ebf7
Emil Bergström <emil.bergstrom@instabox.se>
2021-12-01 10:55:21 +0100

    part 2

--- day01/src/Day01.hs
+++ day01/src/Day01.hs
@@ -1,17 +1,31 @@
 module Day01 where
 
-import Data.List ()
+import Data.List (tails)
 import Data.List.Split (splitOn)
+import System.Environment.MrEnv (envAsString)
+import Text.Read (readMaybe)
 
-maybeBigger :: Integral a => (a, a) -> Bool
--- maybeBigger (Nothing, curr) = False
+maybeBigger :: (Maybe Int, Maybe Int) -> Bool
+maybeBigger (Nothing, curr) = False
 maybeBigger (prev, curr) = curr > prev
 
-countIncreases :: Integral a => [a] -> Int
-countIncreases numbers = length [x | (x, i) <- zip numbers (10000000 : numbers), maybeBigger (i, x)]
+countIncreases :: [Maybe Int] -> Int
+countIncreases numbers = length [x | (x, y) <- zip numbers (Nothing : numbers), maybeBigger (y, x)]
+
+splitNumbers :: [Int] -> [[Int]]
+splitNumbers numbers = map (take 3) (tails numbers)
+
+generatePairs :: [[Int]] -> [([Int], [Int])]
+generatePairs numberGroups = [(x, y) | [x, y] <- map (take 2) (tails numberGroups)]
+
+solve :: String -> [Int] -> Int
+solve "part1" numbers = countIncreases (map Just numbers)
+solve "part2" numbers = length [x | (x, y) <- generatePairs (splitNumbers numbers), sum y > sum x]
+solve x _ = error "Not a valid part"
 
 main :: IO ()
 main = do
+  part <- envAsString "part" "part1"
   input <- readFile "data/input.txt"
   let numbers = map read (splitOn "\n" input)
-  print (countIncreases numbers)
+  print (solve part numbers)
--- day01/test/Day01.test.hs
+++ day01/test/Day01.test.hs
@@ -1,9 +1,9 @@
 import Data.List.Split (splitOn)
-import Day01 (countIncreases)
+import Day01 (generatePairs, solve, splitNumbers)
 import Test.Tasty (TestTree, defaultMain, testGroup)
 import Test.Tasty.HUnit (assertEqual, testCase)
 
-testNumbers :: Integral a => [a]
+testNumbers :: [Int]
 testNumbers =
   [ 199,
     200,
@@ -24,12 +24,21 @@ unitTests :: TestTree
 unitTests =
   testGroup
     "Unit tests"
-    [ countIncreasesTests
+    [ generatePairsTests,
+      countIncreasesTests
+    ]
+
+generatePairsTests :: TestTree
+generatePairsTests =
+  testGroup
+    "generatePairs"
+    [ testCase "should generate pairs" $ assertEqual [] [([1, 2], [2, 3]), ([2, 3], [3, 4]), ([3, 4], [5, 6])] (generatePairs [[1, 2], [2, 3], [3, 4], [5, 6]])
     ]
 
 countIncreasesTests :: TestTree
 countIncreasesTests =
   testGroup
-    "countIncreasesTests"
-    [ testCase "should count increases" $ assertEqual [] 7 (countIncreases testNumbers)
+    "solveTests"
+    [ testCase "should count increases for part1" $ assertEqual [] 7 (solve "part1" testNumbers),
+      testCase "should count increases for part1" $ assertEqual [] 5 (solve "part2" testNumbers)
     ]
