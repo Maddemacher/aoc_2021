23b4d50e76e4029ca1f1b72d70b2d03cc00b769b
Maddemacher <emil.bergstrom1@gmail.com>
2021-12-01 07:42:44 +0100

    p1

--- day01/src/Day01.hs
+++ day01/src/Day01.hs
@@ -2,37 +2,16 @@ module Day01 where
 
 import Data.List ()
 import Data.List.Split (splitOn)
-import System.Environment ()
-import System.Environment.MrEnv (envAsString)
 
-divisors :: Integral a => a -> [a]
-divisors 0 = [1]
-divisors 1 = [1]
-divisors n = [x | x <- [1 .. n], mod n x == 0]
+maybeBigger :: Integral a => (a, a) -> Bool
+-- maybeBigger (Nothing, curr) = False
+maybeBigger (prev, curr) = curr > prev
 
-prime :: Integral a => a -> Bool
-prime n = divisors n == [1, n]
-
-notPrime :: Integral a => a -> Bool
-notPrime n = not (prime n)
-
-getRelevantNumbers :: Integral a => (a -> Bool) -> [a] -> [(a, a)]
-getRelevantNumbers predicate numbers = [(x, i) | (x, i) <- zip numbers [0 ..], predicate x]
-
-partOneAggregate :: Integral a => (a, a) -> a
-partOneAggregate (number, index) = number * index
-
-partTwoAggregate :: Integral a => (a, a) -> a
-partTwoAggregate (number, index) = if even index then number else - number
-
-doPart :: Integral a => String -> [a] -> a
-doPart "part1" numbers = sum (map partOneAggregate (getRelevantNumbers prime numbers))
-doPart "part2" numbers = sum (map partTwoAggregate (getRelevantNumbers notPrime numbers))
-doPart x input = error "Not a valid part"
+countIncreases :: Integral a => [a] -> Int
+countIncreases numbers = length [x | (x, i) <- zip numbers (10000000 : numbers), maybeBigger (i, x)]
 
 main :: IO ()
 main = do
-  part <- envAsString "part" ""
   input <- readFile "data/input.txt"
   let numbers = map read (splitOn "\n" input)
-  print (doPart part numbers)
+  print (countIncreases numbers)
--- day01/test/Day01.test.hs
+++ day01/test/Day01.test.hs
@@ -1,9 +1,21 @@
 import Data.List.Split (splitOn)
-import Day01 (divisors, doPart, prime)
+import Day01 (countIncreases)
 import Test.Tasty (TestTree, defaultMain, testGroup)
 import Test.Tasty.HUnit (assertEqual, testCase)
 
-testNumbers = [0, 3, 4, 42, 106, 107, 267, 269]
+testNumbers :: Integral a => [a]
+testNumbers =
+  [ 199,
+    200,
+    208,
+    210,
+    200,
+    207,
+    240,
+    269,
+    260,
+    263
+  ]
 
 main :: IO ()
 main = defaultMain unitTests
@@ -12,34 +24,12 @@ unitTests :: TestTree
 unitTests =
   testGroup
     "Unit tests"
-    [ divisorsTests,
-      primeTests,
-      partTests
+    [ countIncreasesTests
     ]
 
-divisorsTests :: TestTree
-divisorsTests =
+countIncreasesTests :: TestTree
+countIncreasesTests =
   testGroup
-    "divisors"
-    [ testCase "should return divisors of 0" $ assertEqual [] [1] (divisors 0),
-      testCase "should return divisors of 1" $ assertEqual [] [1] (divisors 1),
-      testCase "should return divisors of 10" $ assertEqual [] [1, 2, 5, 10] (divisors 10)
-    ]
-
-primeTests :: TestTree
-primeTests =
-  testGroup
-    "prime"
-    [ testCase "should return false for 0" $ assertEqual [] False (prime 0),
-      testCase "should return false for 1" $ assertEqual [] False (prime 1),
-      testCase "should return true for prime" $ assertEqual [] True (prime 7),
-      testCase "should return false for non prime" $ assertEqual [] False (prime 10)
-    ]
-
-partTests :: TestTree
-partTests =
-  testGroup
-    "partTests"
-    [ testCase "should return false for 0" $ assertEqual [] 2421 (doPart "part1" testNumbers),
-      testCase "should return false for 0" $ assertEqual [] 335 (doPart "part2" testNumbers)
+    "countIncreasesTests"
+    [ testCase "should count increases" $ assertEqual [] 7 (countIncreases testNumbers)
     ]
