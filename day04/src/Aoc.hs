e1b48918033efdd95244ec15b15151fda488787a
Maddemacher <emil.bergstrom1@gmail.com>
2021-12-04 15:14:38 +0100

    cleanup

--- day04/src/Aoc.hs
+++ day04/src/Aoc.hs
@@ -1,8 +1,6 @@
 module Aoc where
 
-import Control.Monad (forM_, msum)
-import Data.Char (digitToInt)
-import Data.List (find, inits, intercalate, maximumBy, minimumBy, tails, transpose)
+import Data.List (find, inits, maximumBy, minimumBy, transpose)
 import Data.List.Split (splitOn)
 import Data.Ord (comparing)
 import System.Environment.MrEnv (envAsString)
@@ -10,15 +8,15 @@ import System.Environment.MrEnv (envAsString)
 type Board = [[Int]]
 
 sublist :: Eq a => [a] -> [a] -> Bool
-sublist sub universe = all (`elem` universe) sub
+sublist universe = all (`elem` universe)
 
-compareSolutions :: (Foldable t1, Foldable t2) => (t1 a1, b1) -> (t2 a2, b2) -> Ordering
-compareSolutions a b = compare (length (fst a)) (length (fst b))
+compareSolutions :: ([Int], b) -> ([Int], b) -> Ordering
+compareSolutions (a, _) (b, _) = compare (length a) (length b)
 
-hasWinningRow :: [Int] -> Board -> Bool
-hasWinningRow numbers board = do
-  let a = any (`sublist` numbers) board
-  let b = any (`sublist` numbers) (transpose board)
+hasWinningRow :: Board -> [Int] -> Bool
+hasWinningRow board numbers = do
+  let a = any (sublist numbers) board
+  let b = any (sublist numbers) (transpose board)
   a || b
 
 getScore :: (Num a, Foldable t, Eq a) => ([a], t [a]) -> a
@@ -26,37 +24,35 @@ getScore (numbers, board) = do
   let unmarked = [x | x <- concat board, x `notElem` numbers]
   sum unmarked * last numbers
 
-scoreBoard :: [Int] -> [[Int]] -> ([Int], [[Int]])
+scoreBoard :: [Int] -> Board -> ([Int], Board)
 scoreBoard numbers board = do
-  let suggestions = inits numbers
-  let solution = find (`hasWinningRow` board) suggestions
+  let solution = find (hasWinningRow board) (inits numbers)
   case solution of
     Just solution -> (solution, board)
     Nothing -> error "no solution"
 
-scoreBoards :: [Int] -> [[[Int]]] -> [([Int], [[Int]])]
+scoreBoards :: [Int] -> [Board] -> [([Int], Board)]
 scoreBoards numbers = map (scoreBoard numbers)
 
-solve :: [Char] -> [Int] -> [[[Int]]] -> Int
-solve "part1" numbers boards = getScore (minimumBy compareSolutions (scoreBoards numbers boards))
-solve "part2" numbers boards = getScore (maximumBy compareSolutions (scoreBoards numbers boards))
-solve x _ _ = error "Not a valid part"
+solve :: [Char] -> ([Int], [Board]) -> Int
+solve "part1" (numbers, boards) = getScore (minimumBy compareSolutions (scoreBoards numbers boards))
+solve "part2" (numbers, boards) = getScore (maximumBy compareSolutions (scoreBoards numbers boards))
+solve x _ = error "Not a valid part"
 
 constructBoard :: String -> Board
 constructBoard input = do
   let rows = lines input
-  map (\row -> map read (words row)) rows
+  let worded = map words rows
+  map (map read) worded
 
 parseInput :: String -> ([Int], [Board])
 parseInput input = do
-  let groups = splitOn "\n\n" input
-  let numbers = map read (splitOn "," (head groups))
-  let boards = map constructBoard (tail groups)
-  (numbers, boards)
+  let n : bs = splitOn "\n\n" input
+  let numbers = splitOn "," n
+  (map read numbers, map constructBoard bs)
 
 main :: IO ()
 main = do
   part <- envAsString "part" "part1"
   input <- readFile "data/input.txt"
-  let (numbers, boards) = parseInput input
-  print (solve part numbers boards)
+  print (solve part (parseInput input))
--- day04/test/Aoc.test.hs
+++ day04/test/Aoc.test.hs
@@ -3,16 +3,6 @@ import Data.List.Split (splitOn)
 import Test.Tasty (TestTree, defaultMain, testGroup)
 import Test.Tasty.HUnit (assertEqual, testCase)
 
-untilWinningRow = [7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24]
-
-winningBoard =
-  [ [14, 21, 17, 24, 4],
-    [10, 16, 15, 9, 19],
-    [18, 8, 23, 26, 20],
-    [22, 11, 13, 6, 5],
-    [2, 0, 12, 3, 7]
-  ]
-
 main :: IO ()
 main = do
   input <- readFile "data/testInput.txt"
@@ -26,11 +16,11 @@ unitTests input input2 =
     ]
 
 solveTests input input2 = do
-  let (numbers, boards) = (parseInput input)
-  let (numbers2, boards2) = (parseInput input2)
+  let parsed = parseInput input
+  let parsed2 = parseInput input2
   testGroup
     "solveTests"
-    [ testCase "should solve part 1" $ assertEqual [] 4512 (solve "part1" numbers boards),
-      testCase "should solve part 1 with real data" $ assertEqual [] 14093 (solve "part1" numbers2 boards2),
-      testCase "should sovle part 2 with real data" $ assertEqual [] 17388 (solve "part2" numbers2 boards2)
+    [ testCase "should solve part 1" $ assertEqual [] 4512 (solve "part1" parsed),
+      testCase "should solve part 1 with real data" $ assertEqual [] 14093 (solve "part1" parsed2),
+      testCase "should sovle part 2 with real data" $ assertEqual [] 17388 (solve "part2" parsed2)
     ]
