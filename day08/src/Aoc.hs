11c8723aec0be152ed8caea347ab3241abc712e6
Emil Bergström <emil.bergstrom@instabox.se>
2021-12-08 12:48:48 +0100

    part 2

--- day08/src/Aoc.hs
+++ day08/src/Aoc.hs
@@ -1,19 +1,44 @@
 module Aoc where
 
+import Data.Function (on)
 import Data.IntMap (fromListWith, toList)
-import Data.List (group, groupBy, sort)
+import Data.List (group, groupBy, intersect, nub, sort, sortBy)
 import Data.List.Split (splitOn)
 import Data.List.Utils (replace)
 import Data.Maybe (isJust)
 import Debug.Trace (trace)
 import System.Environment.MrEnv (envAsString)
 
+type ParsedInput = ([String], [String])
+
+addDigit num d = 10 * num + d
+
+fromDigits :: [Int] -> Int
+fromDigits = foldl addDigit 0
+
+isZeroOrSixOrNine :: String -> [String] -> Int
+isZeroOrSixOrNine input [one, seven, four] = do
+  if length (intersect one input) == 1
+    then 6
+    else
+      if length (intersect four input) == 4
+        then 9
+        else 0
+isZeroOrSixOrNine _ _ = error "abo"
+
+isTwoOrThreeOrFive :: String -> [String] -> Int
+isTwoOrThreeOrFive input [one, seven, four] = do
+  if length (intersect one input) == 2
+    then 3
+    else
+      if length (intersect four input) == 3
+        then 5
+        else 2
+isTwoOrThreeOrFive _ _ = error "abo"
+
 getDidigt :: [Char] -> Maybe Int
 getDidigt input = do
-  let grouped = groupBy (==) (sort input)
-  case length grouped of
-    0 -> Nothing
-    1 -> Nothing
+  case length input of
     2 -> Just 1
     3 -> Just 7
     4 -> Just 4
@@ -22,20 +47,46 @@ getDidigt input = do
     7 -> Just 8
     x -> error "Hej"
 
-solve :: [Char] -> [String] -> Int
-solve "part1" numbers = length (filter isJust (map getDidigt numbers))
-solve "part2" numbers = 10
-solve x _ = error "Not a valid part"
+getDidigt2 :: [String] -> String -> Int
+getDidigt2 signals input = do
+  case length input of
+    2 -> 1
+    3 -> 7
+    4 -> 4
+    5 -> isTwoOrThreeOrFive input signals
+    6 -> isZeroOrSixOrNine input signals
+    7 -> 8
+    x -> error "Hej"
 
-parseInput :: String -> [String]
-parseInput input = do
+getDidgits :: ParsedInput -> Int
+getDidgits (signals, inputs) = fromDigits (map (getDidigt2 signals) inputs)
+
+getDidgitsFor :: [ParsedInput] -> [Int]
+getDidgitsFor = map getDidgits
+
+parseInput1 :: String -> [String]
+parseInput1 input = do
   let rows = lines input
   let columns = map (splitOn " | ") rows
   let hej = map last columns
-  concat (map (\x -> words x) hej)
+  concatMap words hej
+
+parseRow row = do
+  let [a, b] = splitOn " | " row
+  (take 3 (sortBy (compare `on` length) (words a)), words b)
+
+parseInput2 :: String -> [ParsedInput]
+parseInput2 input = do
+  let rows = lines input
+  map parseRow rows
+
+solve :: [Char] -> String -> Int
+solve "part1" numbers = length (filter isJust (map getDidigt (parseInput1 numbers)))
+solve "part2" numbers = sum (getDidgitsFor (parseInput2 numbers))
+solve x _ = error "Not a valid part"
 
 main :: IO ()
 main = do
   part <- envAsString "part" "part1"
   input <- readFile "data/input.txt"
-  print (solve part (parseInput input))
+  print (solve part input)
