cfe56941ed1f67d92bdf7a79f6f2c946b9e10ae4
Emil Bergström <emil.bergstrom@instabox.se>
2021-12-09 09:02:44 +0100

    part 1

---
+++ day09/src/Aoc.hs
@@ -0,0 +1,64 @@
+module Aoc where
+
+import Control.Exception (try)
+import Data.Char (digitToInt)
+import Data.Function (on)
+import Data.IntMap (fromListWith, toList)
+import Data.List (group, groupBy, intersect, nub, sort, sortBy)
+import Data.List.Split (splitOn)
+import Data.Maybe (catMaybes, fromJust, fromMaybe, isJust, isNothing)
+import Debug.Trace (trace)
+import System.Environment.MrEnv (envAsString)
+
+get :: Int -> [a] -> Maybe a
+get i l = do
+  if i >= 0 && i < length l
+    then Just (l !! i)
+    else Nothing
+
+getValue :: (Int, Int) -> [String] -> Maybe Char
+getValue (x, y) heatmap = do
+  let row = fromMaybe [] (get y heatmap)
+  get x row
+
+getSurroundingPoints :: (Int, Int) -> [String] -> String
+getSurroundingPoints (x, y) heatmap =
+  catMaybes
+    [ getValue (x - 1, y - 1) heatmap,
+      getValue (x, y - 1) heatmap,
+      getValue (x + 1, y - 1) heatmap,
+      getValue (x - 1, y) heatmap,
+      getValue (x + 1, y) heatmap,
+      getValue (x - 1, y + 1) heatmap,
+      getValue (x, y + 1) heatmap,
+      getValue (x + 1, y + 1) heatmap
+    ]
+
+getLowPoint :: (Int, Int) -> [String] -> Maybe Int
+getLowPoint (x, y) heatmap = do
+  let point = digitToInt (fromJust (getValue (x, y) heatmap))
+  let surrounding = map digitToInt (getSurroundingPoints (x, y) heatmap)
+  if point < minimum surrounding
+    then Just point
+    else Nothing
+
+getLowPointsForRow :: (String, Int) -> [String] -> [Int]
+getLowPointsForRow (row, index) heatmap = do
+  let indexedRow = zip row [0 ..]
+  catMaybes (map (\(c, i) -> getLowPoint (i, index) heatmap) indexedRow)
+
+getLowPoints :: [String] -> [Int]
+getLowPoints heatmap = do
+  let indexed = zip heatmap [0 ..]
+  concatMap (`getLowPointsForRow` heatmap) indexed
+
+solve :: [Char] -> [String] -> Int
+solve "part1" numbers = sum (map (+ 1) (getLowPoints numbers))
+solve "part2" numbers = 10
+solve x _ = error "Not a valid part"
+
+main :: IO ()
+main = do
+  part <- envAsString "part" "part1"
+  input <- readFile "data/input.txt"
+  print (solve part (lines input))
---
+++ day09/test/Aoc.test.hs
@@ -0,0 +1,30 @@
+import Aoc (getLowPoint, getSurroundingPoints, solve)
+import Data.List (sort)
+import Test.Tasty (TestTree, defaultMain, testGroup)
+import Test.Tasty.HUnit (assertEqual, testCase)
+
+main :: IO ()
+main = do
+  input <- readFile "data/testInput.txt"
+  defaultMain (unitTests input)
+
+unitTests input =
+  testGroup
+    "Unit tests"
+    [ solveTests input
+    ]
+
+solveTests :: [Char] -> TestTree
+solveTests input = do
+  let parsed = lines input
+  testGroup
+    "solveTests"
+    [ testCase "should solve part 1" $ assertEqual [] 15 (solve "part1" parsed),
+      testCase "should solve part 2" $ assertEqual [] 10 (solve "part2" parsed),
+      testCase "should solve part 2" $ assertEqual [] ['1', '3', '9'] (getSurroundingPoints (0, 0) parsed),
+      testCase "should solve part 2" $ assertEqual [] "21938985" (getSurroundingPoints (1, 1) parsed),
+      testCase "should solve part 2" $ assertEqual [] Nothing (getLowPoint (1, 1) parsed),
+      testCase "should solve part 2" $ assertEqual [] Nothing (getLowPoint (1, 2) parsed),
+      testCase "should solve part 2" $ assertEqual [] (Just 1) (getLowPoint (1, 0) parsed),
+      testCase "should solve part 2" $ assertEqual [] (Just 5) (getLowPoint (2, 2) parsed)
+    ]
