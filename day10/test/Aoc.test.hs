a522ec931d5710be38cd084ce6e70d1cc46b55a2
Maddemacher <emil.bergstrom1@gmail.com>
2021-12-11 14:27:16 +0100

    fixing tests

--- day10/test/Aoc.test.hs
+++ day10/test/Aoc.test.hs
@@ -1,4 +1,4 @@
-import Aoc (getLowPoint, getSurroundingPoints, solve)
+import Aoc (solve)
 import Data.List (sort)
 import Test.Tasty (TestTree, defaultMain, testGroup)
 import Test.Tasty.HUnit (assertEqual, testCase)
@@ -19,12 +19,6 @@ solveTests input = do
   let parsed = lines input
   testGroup
     "solveTests"
-    [ testCase "should solve part 1" $ assertEqual [] 15 (solve "part1" parsed),
-      testCase "should solve part 2" $ assertEqual [] 10 (solve "part2" parsed),
-      testCase "should solve part 2" $ assertEqual [] ['1', '3', '9'] (getSurroundingPoints (0, 0) parsed),
-      testCase "should solve part 2" $ assertEqual [] "21938985" (getSurroundingPoints (1, 1) parsed),
-      testCase "should solve part 2" $ assertEqual [] Nothing (getLowPoint (1, 1) parsed),
-      testCase "should solve part 2" $ assertEqual [] Nothing (getLowPoint (1, 2) parsed)
-      -- testCase "should solve part 2" $ assertEqual [] (Just 1) (getLowPoint (1, 0) parsed),
-      -- testCase "should solve part 2" $ assertEqual [] (Just 5) (getLowPoint (2, 2) parsed)
+    [ testCase "should solve part 1" $ assertEqual [] 26397 (solve "part1" parsed),
+      testCase "should solve part 2" $ assertEqual [] 288957 (solve "part2" parsed)
     ]
